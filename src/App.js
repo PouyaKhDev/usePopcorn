f8606efcc8be90fabbeebea772388b51741bbead
pouyaxanony <pouyakhajavidev@gmail.com>
2024-07-24 00:58:40 +0330

    static parts are done and components are splited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,241 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <NavigationBar>
+        <Logo />
+        <InputField placeHolder="Search a movie..." />
+        <SearchResultsNum movies={movies} />
+      </NavigationBar>
+
+      <main className="main">
+        <SearchResultList movies={movies} />
+
+        <WatchedMoviesContainer />
+      </main>
+    </>
+  );
+}
+
+function NavigationBar({ children }) {
+  return <nav className="nav-bar">{children}</nav>;
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
     </div>
   );
 }
 
-export default App;
+function SearchResultsNum({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies?.length}</strong> results
+    </p>
+  );
+}
+
+function SearchResultList({ movies }) {
+  const [isOpen1, setIsOpen1] = useState(true);
+
+  return (
+    <div className="box">
+      <Btn onClick={() => setIsOpen1((open) => !open)}>
+        {isOpen1 ? "–" : "+"}
+      </Btn>
+
+      {isOpen1 && (
+        <ul className="list">
+          {movies?.map((movie) => (
+            <SearchedMovie movie={movie} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+function SearchedMovie({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+function WatchedMoviesContainer({}) {
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  return (
+    <div className="box">
+      <Btn onClick={() => setIsOpen2((open) => !open)}>
+        {isOpen2 ? "–" : "+"}
+      </Btn>
+
+      {isOpen2 && (
+        <>
+          <WatchedMoviesSum watched={watched} />
+
+          <WatchedMoviesList watched={watched} />
+        </>
+      )}
+    </div>
+  );
+}
+
+function WatchedMoviesSum({ watched }) {
+  const ImdbRatingArr = watched.map((movie) => movie.imdbRating);
+  const userRatingArr = watched.map((movie) => movie.userRating);
+  const runtimeArr = watched.map((movie) => movie.runtime);
+
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <DisplayAverage>{ImdbRatingArr}</DisplayAverage>
+        </p>
+        <p>
+          <span>🌟</span>
+          <DisplayAverage>{userRatingArr}</DisplayAverage>
+        </p>
+        <p>
+          <span>⏳</span>
+          <DisplayAverage>{runtimeArr}</DisplayAverage>
+          <span>min</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function WatchedMoviesList({ watched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+function WatchedMovie({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+function Btn({ onClick, children }) {
+  return (
+    <button className="btn-toggle" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+function DisplayAverage({ children }) {
+  return (
+    <span>
+      {children.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)}
+    </span>
+  );
+}
+
+function InputField({ placeHolder }) {
+  const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder={placeHolder}
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
